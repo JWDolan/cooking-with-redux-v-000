bcac64bed6da92489394a3fca7df2eb691c4b341
James Dolan <jwdolan820@gmail.com>
2017-07-20 02:53:21 +0000

    Done.

--- src/components/ingredients/IngredientsInput.js
+++ src/components/ingredients/IngredientsInput.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {createIngredient} from '../../actions/ingredients';
 
 export class IngredientsInput extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: '',
+      calories: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCalorieChange = this.handleCalorieChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleCalorieChange(e){
+    this.setState({
+      calories: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const {createIngredient} = this.props;
+    createIngredient({name: this.state.text, calories: this.state.calories});
+    this.setState({text: '', calories: ''});
+  }
+
   render(){
     return(
-      <div>
-        Ingredients Input
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.text} onChange={this.handleTextChange} />
+        <input type="text" value={this.state.calories} onChange={this.handleCalorieChange} />
+        <input type="submit" value="Create Ingredient" />
+      </form>
     )
   }
 }
 
-export const ConnectedIngredientsInput = IngredientsInput
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.ingredients
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    createIngredient: createIngredient
+  }, dispatch);
+}
+
+export const ConnectedIngredientsInput = connect(mapStateToProps, mapDispatchToProps)(IngredientsInput);
